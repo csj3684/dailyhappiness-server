088a2f0ca5dc7fdf096433361204249a5f8dcb31
Please Log Out After Use <37976615+csj3684@users.noreply.github.com>
2019-12-02 19:44:07 +0900

    Update main.py

--- main.py
+++ main.py
@@ -34,14 +34,14 @@ while True:
     if action == 1: 
         functions.set_user_applicable_missions(target_user_id, classified_R_hat, user_info, mission_info, weekly_weather)
     elif action == 2:
-        set_weekly_mission(target_user_id, user_info, mission_info, functions.get_today_idx(), weekly_weather)
+        functinos.set_weekly_mission(target_user_id, user_info, mission_info, functions.get_today_idx(), weekly_weather)
     elif action == 3:
         daily_mission = functions.get_daily_mission(target_user_id, user_info, today_weather)
         print("daily_mission", daily_mission)
     elif action == 4:
-        update_user_applicable_missions(target_user_id, user_info, daily_mission, "done", functions.get_today_idx(), weekly_weather)
+        functinos.update_user_applicable_missions(target_user_id, user_info, daily_mission, "done", functions.get_today_idx(), weekly_weather)
     elif action == 5:
-        update_user_applicable_missions(target_user_id, user_info, daily_mission, "pass", functions.get_today_idx(), weekly_weather)
+        functinos.update_user_applicable_missions(target_user_id, user_info, daily_mission, "pass", functions.get_today_idx(), weekly_weather)
     else:
         print("Key Error")
         continue
