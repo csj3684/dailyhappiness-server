1e9158907a72f2da5ad22725bb2bdbfa08a32f3a
j03y14 <j03y14@gmail.com>
2019-12-11 03:15:55 +0900

    Update DB.py

--- app/main/DB.py
+++ app/main/DB.py
@@ -1,18 +1,18 @@
-import pymysql
+import mysql.connector
 
 class DB:
     conn=0
     curs=0
     
-    @staticmethod
-    def dbConnect():
-        DB.conn = pymysql.connect(host='dailyhappiness.cglqv9cus9nr.ap-northeast-2.rds.amazonaws.com', user='admin', password='', db='dailyhappiness', charset='utf8')
+    
+    def dbConnect(self):
+        self.conn = mysql.connector.connect(host='dailyhappiness.cglqv9cus9nr.ap-northeast-2.rds.amazonaws.com', user='admin', password='', db='dailyhappiness', charset='utf8')
         
-    @staticmethod    
-    def setCursorDic():
-        DB.curs = DB.conn.cursor(pymysql.cursors.DictCursor)
+      
+    def setCursorDic(self):
+        self.curs = self.conn.cursor(dictionary=True)
         
-    @staticmethod    
-    def dbDisconnect():
-        DB.conn.close()
+   
+    def dbDisconnect(self):
+        self.conn.close()
 
